d4a1bc338c342e25543dcbea934e6eb3dac9de1f
mariamnozadze <mariamnozadze97@yahoo.com>
2022-06-27 18:37:13 +0400

    one page website with small animation

---
+++ index.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("openBtn").style.visibility = "hidden";
+}
+
+/*  width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+  document.getElementById("openBtn").style.visibility = "visible";
+}
